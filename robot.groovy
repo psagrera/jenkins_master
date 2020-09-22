dde0cb8751f092ed68345ee86155403a3a12556a
Your Name <you@example.com>
2020-09-22 20:14:42 +0530

    modifying System Property to allow open windows in html pages

--- robot.groovy
+++ robot.groovy
@@ -5,5 +5,5 @@ def jenkinsHome = Jenkins.instance.getRootDir().absolutePath
 def logger = LogManager.getLogManager().getLogger("")
 /* Replace the Key and value with the values you want to set.*/
 /* System.setProperty(key, value) */
-System.setProperty("hudson.model.DirectoryBrowserSupport.CSP","sandbox allow-scripts; default-src 'none'; img-src 'self' data: ; style-src 'self' 'unsafe-inline' data: ; script-src 'self' 'unsafe-inline' 'unsafe-eval' ;")
+System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
 logger.info("Jenkins Startup Script: Successfully updated the system properties value for hudson.model.DirectoryBrowserSupport.CSP . Script location : ${jenkinsHome}/init.groovy.d ")
